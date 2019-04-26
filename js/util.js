143f7dd7cc252b6431cd63024790da521cce4ac9
Ian Leeder <i_leeder@hotmail.com>
2019-04-26 23:18:17 +1000

    Started work on parsing object file

--- js/util.js
+++ js/util.js
@@ -1,39 +1,72 @@
-function readSingleFile(e) {
-	var file = e.target.files[0];
+function readSingleFile(buffer) {
+	let dv = new DataView(buffer);
+	// displayContents(contents);
+	let score = dv.getUint16(0, true);
+	let goal = dv.getUint16(2, true);
+
+	console.log("Goal = "+goal);
+	let grenades = dv.getUint16(4, true);
+	let grenadeDelay = dv.getUint16(6, true);
+	let field = new Uint8Array(800);
+	for(let i=0, off=8;i<field.length;i++, off++) {
+		field[i] = dv.getUint8(off);
+	}
+	analyzeField(field);
+}
+
+function parseDataFile(buffer) {
+	/*
+	Each tile is 134 bytes.
+	4x Magic Header (00 0F 00 0F)
+	128 bytes
+	2x Magic Footer (90 21)
+	*/
+	let tileSize = 134;
+	
+	// There are 16 tiles (total file size 2144 bytes)
+	if(buffer.byteLength != 2144) {
+		alert("Invalid data file.");
+		return;
+	}
+
+	let begin = 0;
+	for(let i=0;i<16;i++) {
+		parseSprite(buffer.slice(begin, begin + tileSize));
+		begin += tileSize;
+	}
+}
+
+function parseSprite(buffer) {
+	let view = new Uint8Array(buffer);
+	console.log("view length = " + view.length);
+	console.log("view bytelength = " + view.byteLength);
+}
+
+function openFile(e)
+{
+	let file = e.target.files[0];
 	if (!file) {
 		return;
 	}
-	var reader = new FileReader();
+
+	let reader = new FileReader();
 	reader.onload = function(e) {
-		var buffer = e.target.result;
-		var dv = new DataView(buffer);
-
-		// displayContents(contents);
-		var score = dv.getUint16(0, true);
-		var goal = dv.getUint16(2, true);
-
-		console.log("Goal = "+goal);
-		var grenades = dv.getUint16(4, true);
-		var grenadeDelay = dv.getUint16(6, true);
-		var field = new Uint8Array(800);
-		for(var i=0, off=8;i<field.length;i++, off++) {
-			field[i] = dv.getUint8(off);
-		}
-		analyzeField(field);
+		let buffer = e.target.result;
+		parseDataFile(buffer)
 	};
 	reader.readAsArrayBuffer(file);
 }
 
 function analyzeField(f) {
-	var distribution = new Array(16);
-	for(var i=0;i<distribution.length;i++)
+	let distribution = new Array(16);
+	for(let i=0;i<distribution.length;i++)
 		distribution[i] = 0;
 
-	for(var i=0;i<f.length;i++) {
+	for(let i=0;i<f.length;i++) {
 		distribution[f[i]]++;
 	}
 	console.log("Length = " + f.length);
-	var totalAvailable = distribution[3] + (5*distribution[10]);
+	let totalAvailable = distribution[3] + (5*distribution[10]);
 	console.log("Total available = " + totalAvailable);
 	console.log("Empty space = " + distribution[0] + " (" + (distribution[0]/8) + ")%");
 	console.log("Dirt = " + distribution[1] + " (" + (distribution[1]/8) + ")%");
@@ -45,14 +78,14 @@ function analyzeField(f) {
 }
 
 function displayContents(contents) {
-	var element = document.getElementById('file-content');
+	let element = document.getElementById('file-content');
 	element.innerHTML = contents;
 }
 
 if (window.File && window.FileReader && window.FileList && window.Blob) {
 	// Great success! All the File APIs are supported.
 	document.getElementById('file-input')
-	.addEventListener('change', readSingleFile, false);
+	.addEventListener('change', openFile, false);
 } else {
 	alert('The File APIs are not fully supported in this browser.');
 }
