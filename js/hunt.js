78894f6cdcb865c117081a386b67f42f61c7676c
Ian Leeder <i_leeder@hotmail.com>
2022-04-09 00:12:30 +1000

    Clean up index.html and fork into dev.html

--- js/hunt.js
+++ js/hunt.js
@@ -165,10 +165,12 @@ class EmeraldHunt {
 
 		// This is just debug fluff
 		const imageDiv = document.getElementById('imagesDiv');
-		imageDiv.innerHTML = '';
-		EmeraldHunt.IMAGES.forEach((item) => {
-			imageDiv.appendChild(item);
-		});
+		if(imageDiv) {
+			imageDiv.innerHTML = '';
+			EmeraldHunt.IMAGES.forEach((item) => {
+				imageDiv.appendChild(item);
+			});
+		}
 	}
 
 	async resetImageSource() {
